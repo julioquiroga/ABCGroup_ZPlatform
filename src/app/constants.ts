77d9628509ce4efeebdaf005bed0b7eaf91c3f9c
Julio Quiroga <julioquiroga@hotmail.com>
2024-04-17 18:16:51 -0600

    update env

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -3,7 +3,7 @@ export const API_URL = `${import.meta.env.SITE}${import.meta.env.BASE_URL}api/`;
 export const REMOTE_ASSETS_BASE_URL = `https://flowbite-admin-dashboard.vercel.app`;
 
 //export const SITE_TITLE = 'Flowbite Astro Admin Dashboard';
-export const SITE_TITLE = 'ABC Group Z Platform 1.0 Panel Dashboard';
+export const SITE_TITLE = 'ABC Group Z Platform 1.0 Dev Panel Dashboard';
 
 /* Useful flag for sourcing from `./data` entirely, disabling randomize layer */
 export const RANDOMIZE = Boolean(import.meta.env.RANDOMIZE) || true;
